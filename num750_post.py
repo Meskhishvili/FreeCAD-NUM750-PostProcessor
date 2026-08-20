b707e978782b5f822715f15a4d4f20b717f4554e
Malkhaz <85291826+Meskhishvili@users.noreply.github.com>
2026-08-20 19:57:02 +0300

    Update num750_post.py
       gcode += "%999 (SUPER_TEST_VERSION;1)\n"

--- num750_post.py
+++ num750_post.py
@@ -164,7 +164,7 @@ def export(objectslist, filename, argstring):
 
     # write header for NUM750
     if OUTPUT_HEADER:
-        gcode += "%000 (PROGRAM_NAME;1)\n"
+        gcode += "%001 (PROGRAM_NAME;1)\n"
         gcode += "E60000=-070000\n"
         gcode += "E61000=-177000\n"
         gcode += "E62000=-150000\n"
